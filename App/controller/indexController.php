f446eb699150e5439de7b1797277bbd97ff087ed
OrangeGlaze <yu786808630@163.com>
2017-04-12 09:36:29 +0800

    yy

--- App/controller/indexController.php
+++ App/controller/indexController.php
@@ -1,23 +1,50 @@
 <?php
 namespace App\controller;
-<<<<<<< HEAD
-use core\Framework;
 
-class indexController extends Framework{
-    public function index(){
-        $this->display('default/search.html');
-//        include 'default/search.html';
-    }
-}
-=======
+use cord\Framework;
 
-use Core\Framework;
+
 
 class indexController extends Framework
 {
+    //闪屏
+    public function splash(){
+        $this->display('');
+    }
     //首页
     public function index(){
+        $this->display('default/yd/index.html');
     }
-    //
+    //登录
+    public function login(){
+        $this->display('');
+    }
+    //注册
+    public function register(){
+        $this->display('');
+    }
+    //搜索首页
+    public function search(){
+        $this->display('default/GE/search.html');
+    }
+    //私人订制
+    public function tailor(){
+        $this->display('');
+    }
+    //个人中心
+    public function personal(){
+        $this->display('default/wzj_personal/personal.html');
+    }
+    public function follow(){
+        $this->display('default/wzj_personal/follow.html');
+    }
+    public function follow_detail(){
+        $this->display('default/wzj_personal/follow_detail.html');
+    }
+    //旅游圈
+    public function travel(){
+        $this->display('');
+
+    }
 }
->>>>>>> ff6798f4fd63382c7af80a2b4b71f52ba97af8cb
+
--- index.php
+++ index.php
@@ -1,32 +1,21 @@
 <?php
-<<<<<<< HEAD
 define('VIEW','App/views/');
 define('CONTROLLER','App/controller/');
 define('MODEL','App/model/');
-define('CSS_PATH','/Static/css/');
-define('JS_PATH','/Static/js/');
-define('IMG_PATH','/Static/img/');
-define('FONT_PATH','/Static/fonts/');
+define('CSS_PATH','/Static/default/css/');
+define('JS_PATH','/Static/default/js/');
+define('IMG_PATH','/Static/default/images/');
+define('FONT_PATH','/Static/default/fonts/');
 
-require 'core/framework.php';
-require 'core/dbpdo.php';
-require 'core/common.php';
 
-\core\Framework::start();
-=======
-define('CONTROLLER', 'App/controller/');
-define('MODEL', 'App/model/');
-define('VIEWS', 'App/views/');
-define('CSS_PATH', '/Static/css/');
-define('JS_PATH', '/Static/js/');
-define('FONT_PATH', '/Static/fonts/');
-define('MUSIC_PATH', '/Static/musics/');
-define('IMG_PATH', '/Static/images/');
-
-require 'Core/common.php';
+require 'Cord/common.php';
 //require '/DBPdo.php';
-require 'Core/Framework.php';
+require 'Cord/Framework.php';
 
-\Core\Framework::start();
+Cord\Framework::start();
 
->>>>>>> ff6798f4fd63382c7af80a2b4b71f52ba97af8cb
+
+
+
+
+
